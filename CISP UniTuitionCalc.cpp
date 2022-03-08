0c188064bfef09ddffe2590b37f4a26df1d74a89
Noah Goss <95068655+RainbowCitrine@users.noreply.github.com>
2022-03-08 12:09:29 -0800

    Update CISP UniTuitionCalc.cpp

--- CISP UniTuitionCalc.cpp
+++ CISP UniTuitionCalc.cpp
@@ -7,24 +7,24 @@ int main()
     
     double tuition = 10000; //Already paid the first year
     
-    for (int i = 1; i <= 10; i++) //Start at one since you already started at 10000 for the first year 
+    for (int i = 1; i <= 10; i++)
     {
-        tuition *= 1.05;
+        tuition = tuition * 1.05;
     }
 
     cout << "Tuition in ten years is " << tuition << endl; 
    
     double sum = tuition; //Total of the 10th year 
-    for (int i = 11; i < 14; i++) //Wanted us to calculate the 4 years after 10th year 
+    for (int i = 2; i <= 4; i++)
     {
         
-        tuition = tuition * 1.05; 
-        sum += tuition * 1.05; 
+        tuition = tuition * 1.05;
+        sum += tuition; 
        
         
          
     }
     
-    cout << "The four year tuition in ten years is " << sum << endl; 
-
+    cout << "The four year tuition in ten years is " << sum << endl;
+    
 }
